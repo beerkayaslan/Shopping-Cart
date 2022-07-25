a30bd4fa62c59621c13edef94da0af8572c95973
beerkayaslan <46554154+beerkayaslan@users.noreply.github.com>
2022-07-25 07:14:00 +0300

    README.md update

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import LogoImg from "theme/logo.png";
 
 export default function Logo() {
     return (
         <Link to="/">
-            <img className="w-[150px]" src="https://shop-pickbazar-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F860%2FPickBazar.png&w=1920&q=75" />
+            <img className="w-[80px] h-[30px]" src={LogoImg} />
         </Link>
     )
 }
